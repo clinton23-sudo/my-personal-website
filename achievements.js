25c53ce3437882bc6cc7625283aa74e466739345
clinton23-sudo <dabbaang23@gmail.com>
2026-09-16 19:59:54 +0200

    Create achievements.js

---
+++ achievements.js
@@ -0,0 +1,14 @@
+const achievements = [
+    {
+        title: "Academic Achievement",
+        description: "A record of my academic progress and achievements."
+    },
+    {
+        title: "School Activities",
+        description: "Participation in school activities, projects and events."
+    },
+    {
+        title: "Personal Projects",
+        description: "Creative and educational projects that I have worked on."
+    }
+];
