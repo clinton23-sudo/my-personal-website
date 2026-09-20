3c786b7a282769e3b2af4de97603cf78757f0efb
clinton23-sudo <dabbaang23@gmail.com>
2026-09-20 22:11:15 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,3 @@
-```javascript
 const websiteData = {
     name: "Clinton Ncube",
 
@@ -14,4 +13,79 @@ const websiteData = {
     lastUpdated:
         "2026"
 };
-```
+
+
+// ===============================
+// GALLERY
+// ===============================
+
+const gallery = [
+    {
+        title: "Gallery Photo 1",
+        image: "images/IMG-20240819-WA0011_(1).jpg"
+    },
+    {
+        title: "Gallery Photo 2",
+        image: "images/IMG-20240819-WA0020_(1).jpg"
+    },
+    {
+        title: "Gallery Photo 3",
+        image: "images/IMG-20240819-WA0021_(1).jpg"
+    },
+    {
+        title: "Gallery Photo 4",
+        image: "images/IMG-20251221-WA0056.jpg"
+    },
+    {
+        title: "Gallery Photo 5",
+        image: "images/IMG_20250802_093252.jpg"
+    },
+    {
+        title: "Gallery Photo 6",
+        image: "images/IMG_20250802_093305.jpg"
+    },
+    {
+        title: "Gallery Photo 7",
+        image: "images/IMG_20250802_144742_edit_7426347801541.jpg"
+    },
+    {
+        title: "Gallery Photo 8",
+        image: "images/IMG_20250802_145115_edit_7303969500494.jpg"
+    },
+    {
+        title: "Gallery Photo 9",
+        image: "images/IMG_20250802_145348_edit_7272302860610.jpg"
+    },
+    {
+        title: "Gallery Photo 10",
+        image: "images/WIN_20260814_16_48_08_Pro.jpg"
+    },
+    {
+        title: "Gallery Photo 11",
+        image: "images/WIN_20260814_16_48_27_Pro.jpg"
+    },
+    {
+        title: "Gallery Photo 12",
+        image: "images/WIN_20260814_16_48_35_Pro.jpg"
+    },
+    {
+        title: "Gallery Photo 13",
+        image: "images/WIN_20260814_16_48_39_Pro.jpg"
+    },
+    {
+        title: "Gallery Photo 14",
+        image: "images/WIN_20260814_16_49_03_Pro.jpg"
+    },
+    {
+        title: "Gallery Photo 15",
+        image: "images/WIN_20260814_16_49_16_Pro.jpg"
+    },
+    {
+        title: "Gallery Photo 16",
+        image: "images/WIN_20260814_16_49_28_Pro.jpg"
+    },
+    {
+        title: "Gallery Photo 17",
+        image: "images/photo3.jpg"
+    }
+];
